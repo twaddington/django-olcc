3f54454c2eec9378d7bef836f37967c044f88faa
Tristan Waddington <tristan.waddington@gmail.com>
2012-01-06 00:46:03 -0800

    Clean up the management command stub in preparation of further development.

--- django_olcc/olcc/management/commands/olccimport.py
+++ django_olcc/olcc/management/commands/olccimport.py
@@ -1,9 +1,14 @@
 import os
 import xlrd
+
 from django.core.management.base import BaseCommand, CommandError
+
+from olcc.models import Product
 
 class Command(BaseCommand):
     """
+    :todo: Use optparse to add a --quiet option to supress all output except errors.
+    :todo: Write a separate management command to fetch the latest price document.
     """
     args = "<filename>"
     help = "Parses an excel document of OLCC price data."
@@ -18,19 +23,21 @@ class Command(BaseCommand):
             self.stdout.write("Importing from \"%s\"...\n" % filename)
 
             # Import workbook
-            wb = xlrd.open_workbook(filename);
+            wb = xlrd.open_workbook(filename)
             self.stdout.write("Sheet Names:\n%s\n" % wb.sheet_names())
 
             # Get the first sheet
             sheet = wb.sheet_by_index(0)
 
             # Loop over worksheet
-            #[u'Report Date:  ', '', 40863.0, '', '', '']
-            #['', '', '', '', '', '']
-            #[u'Item Code', u'Item Status', u'Description', u'Size', u'Bottles per Case', u'Bottle Price']
-            #[u'0102B', u'@', u'GLENFIDDICH SNOW PHOENIX', u'750 ML', 6.0, 92.950000000000003]
             for rownum in range(sheet.nrows):
-                print sheet.row_values(rownum)
+                values = sheet.row_values(rownum)
+                if len(values) > 0:
+                    try:
+                        # TODO: Updating products
+                        Product.from_row(values)
+                    except IndexError:
+                        pass
         except IOError as (errno, strerror):
             raise CommandError("%s" % strerror)
         except IndexError:
