a949473350fe193c84ea85551e68117b54e08b94
Tristan Waddington <tristan.waddington@gmail.com>
2012-01-26 00:40:07 -0800

    Enable the Django admin and wire up a simple robots.txt.

--- django_olcc/urls.py
+++ django_olcc/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# Enable the django admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Wire up the admin urls
+    url(r'^admin/', include(admin.site.urls)),
+
+    # robots.txt
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )
