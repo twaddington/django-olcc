577f55fa0a083cd11f8b2c2ed11ef8d9ab483a39
Tristan Waddington <tristan.waddington@gmail.com>
2012-04-29 16:59:17 -0700

    Add gunicorn to requirements.txt and INSTALLED_APPS. Create Procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn_django -b 0.0.0.0:$PORT -w 3
--- django_olcc/settings.py
+++ django_olcc/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'olcc',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
