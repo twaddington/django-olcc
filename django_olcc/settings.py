9de6906d1c1359addc6147ef1bc80ed1c7b1c589
Tristan Waddington <tristan.waddington@gmail.com>
2012-05-05 17:22:41 -0700

    Load the humanize app.

--- django_olcc/settings.py
+++ django_olcc/settings.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.humanize',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'olcc',
