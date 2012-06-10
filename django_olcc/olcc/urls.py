d514a8cdf9e7b6b236b9da6cb8dad7fa6d9be8af
Tristan Waddington <tristan.waddington@gmail.com>
2012-06-10 00:51:18 -0700

    Update the view names and url paths. Stub out the on sale products list and about page.

--- django_olcc/olcc/urls.py
+++ django_olcc/olcc/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('olcc.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^p/(?P<page>\d+)/$', 'product_list', name='products'),
-    url(r'^(?P<slug>[-\w]+)/$', 'product_detail', name='product'),
-    url(r'^s/(?P<page>\d+)/$', 'store_list', name='stores'),
+    url(r'^$', 'home_view', name='home'),
+    url(r'^stores/$', 'store_list_view', name='stores'),
+    url(r'^products/(?P<page>\d+)/$', 'product_list_view', name='products'),
+    url(r'^sale/(?P<page>\d+)/$', 'sale_list_view', name='sale'),
+    url(r'^p/(?P<slug>[-\w]+)/$', 'product_view', name='product'),
+
+    # Static about page
+    url(r'^about/$', direct_to_template,
+        {'template': 'olcc/about.html',}, name='about'),
 )
--- django_olcc/olcc/views.py
+++ django_olcc/olcc/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 
 from olcc.models import Product, ProductPrice, Store
 
-def home(request):
+def home_view(request):
     """
     The site landing page.
     """
@@ -13,7 +13,7 @@ def home(request):
     return render_to_response('olcc/home.html',
             context, context_instance=RequestContext(request))
 
-def product_list(request, page=1):
+def product_list_view(request, page=1):
     """
     Display a paginated list of products.
     """
@@ -33,7 +33,27 @@ def product_list(request, page=1):
     return render_to_response('olcc/product_list.html',
             context, context_instance=RequestContext(request))
 
-def product_detail(request, slug):
+def sale_list_view(request, page=1):
+    """
+    Display a paginated list of on sale products.
+    """
+    per_page = int(request.GET.get('pp', 25))
+    products = Product.objects.on_sale().order_by('title')
+
+    p = Paginator(products, per_page)
+    try:
+        products_page = p.page(page)
+    except InvalidPage:
+        raise Http404
+    
+    context = {
+        'products_page': products_page, 
+    }
+
+    return render_to_response('olcc/product_list.html',
+            context, context_instance=RequestContext(request))
+
+def product_view(request, slug):
     """
     Display product details.
     """
@@ -49,21 +69,15 @@ def product_detail(request, slug):
     return render_to_response('olcc/product.html',
             context, context_instance=RequestContext(request))
 
-def store_list(request, page=1):
+def store_list_view(request):
     """
-    Display a paginated list of stores.
+    Display a list of stores.
     """
-    per_page = int(request.GET.get('pp', 25))
-    stores = Store.objects.all().order_by('name')
+    # Order by county, then name
+    stores = Store.objects.all().order_by('county', 'name')[:5]
 
-    p = Paginator(stores, per_page)
-    try:
-        store_page = p.page(page)
-    except InvalidPage:
-        raise Http404
-    
     context = {
-        'store_page': store_page,
+        'stores': stores,
     }
 
     return render_to_response('olcc/stores.html',
