4782fc2b7884d34d25e10a083f469afd15837cc5
Tristan Waddington <tristan.waddington@gmail.com>
2012-06-09 18:34:58 -0700

    Update urls.py with the necessary staticfiles boilerplate.

--- django_olcc/urls.py
+++ django_olcc/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
 
 # Enable the django admin
@@ -12,6 +14,14 @@ urlpatterns = patterns('',
     # Wire up the admin urls
     url(r'^admin/', include(admin.site.urls)),
 
+    # Static files
+    #(r'^static/(?P<static>.*)$', 'django.views.static.serve',
+    #    {'document_root': settings.STATIC_ROOT}),
+
     # robots.txt
-    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    (r'^robots\.txt$', direct_to_template,
+        {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )
+
+# Static files
+urlpatterns += staticfiles_urlpatterns()
