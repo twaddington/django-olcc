cdef62151437e9bf1187b955ad212d9d32e007e3
Tristan Waddington <tristan.waddington@gmail.com>
2012-06-10 23:55:05 -0700

    Tweak the tastypie resource configuration.

--- django_olcc/olcc/api.py
+++ django_olcc/olcc/api.py
@@ -1,17 +1,34 @@
-from tastypie.resources import ModelResource
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL
 from olcc.models import Product, ProductPrice, Store
 
 class ProductResource(ModelResource):
     class Meta:
         queryset = Product.objects.all()
         resource_name = 'product'
+        allowed_methods = ['get']
+        filtering = {
+            'title': ALL,
+            'code': ALL,
+            'proof': ALL,
+            'size': ALL,
+            'status': ALL,
+            'on_sale': ALL,
+        }
 
 class ProductPriceResource(ModelResource):
+    product = fields.ToOneField(ProductResource, 'product')
+
     class Meta:
         queryset = ProductPrice.objects.all()
         resource_name = 'price'
+        allowed_methods = ['get']
+        filtering = {
+            'product': ['exact'],
+        }
 
 class StoreResource(ModelResource):
     class Meta:
         queryset = Store.objects.all()
         resource_name = 'store'
+        allowed_methods = ['get']
